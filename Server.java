5194fc8a46f4f35b09f5043ed32f64fff5c86804
Anoop S <anoop.shivayogi@gmail.com>
2022-10-12 23:50:42 -0700

    HandleRequest completed

--- Server.java
+++ Server.java
@@ -1,12 +1,14 @@
 import java.io.*;
 import java.util.*;
 import java.net.*;
+import java.text.SimpleDateFormat;
 
 public class Server
 {
     public static void main(String args[]) throws IOException
     {
-        if(args.length != 4){
+        if(args.length != 4)
+        {
             System.out.println("Required 4 arguments");
             System.exit(0);
         }
@@ -46,14 +48,79 @@ class HttpRequest implements Runnable
             System.out.println(e);
         }
     }
-}
 
 
-private void handleRequest() throws IOException{
-    BufferReader in = new BufferReader(new InputStreamReader(socket.getInputStream()));
-    PrintStream o = new PrintStream(new BufferReader(socket.getOutputStream()));
+    private void handleRequest() throws IOException
+    {
+        // Get a reference to the socket's input and output streams.
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintStream o = new PrintStream(new BufferedOutputStream(socket.getOutputStream()));
 
-    String command = in.readLine();
-    System.out.println();
-    
+        String command = in.readLine(); // command is the first Request line (get /path/file.html version of http) of the HTTP request message.
+        System.out.println();
+        System.out.println(command);  // printing  the first Request line.
+
+        String file="";
+        StringTokenizer tokens = new StringTokenizer(command);
+        try{
+
+            //Parse the file from the GET command
+            if (tokens.hasMoreElements() && tokens.nextToken().equalsIgnoreCase("GET") && tokens.hasMoreElements())
+                file = tokens.nextToken();
+            else
+            throw new FileNotFoundException(); // Bad request
+
+        if (file.endsWith("/")) //Append "/" with "index.html"
+            file += "index.html";
+
+        while (file.indexOf("/") == 0) //Remove leading / from filename
+            file = file.substring(1);
+
+            if((new File(file).exists()) && !(new File(file).canRead()))
+            {
+                o.print("HTTP/1.0 403 Forbidden\r\n"+
+                "Location: /"+file+"/\r\n\r\n");
+                o.close();
+                return;
+            }
+
+        FileInputStream f = new FileInputStream(file);
+        SimpleDateFormat format1 = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:Ss z");
+
+        // Determine the content type and print HTTP header
+        String content = "text/plain";
+        if (file.endsWith(".html") || file.endsWith(".htm"))
+            content = "text/html";
+        else if (file.endsWith(".jpg") || file.endsWith(".jpeg"))
+            content = "image/jpeg";
+        else if (file.endsWith(".gif"))
+            content = "image/gif";
+        else if (file.endsWith(".class"))
+            content = "application/octet-stream";
+        o.print("HTTP/1.0 200 OK\r\n"+
+            "Content-type: "+content+"\r\n\r\n");
+
+        o.print("HTTP/1.0 200 OK\r\n"+"Content-length:"+(new File(file)).length()+"\r\n\r\n"+"Date: "+format1.format(new Date())+"\r\n\r\n");
+        System.out.println("HTTP/1.0 200 OK\r\n");
+
+        byte buffer[]=new byte[5000];
+        int i;
+        while ((i=f.read(buffer))>0)
+        o.write(buffer, 0, i);
+        o.close();
+        socket.close();
+        System.out.println("connection closed");
+        
+        }
+        catch (FileNotFoundException fnfe) 
+        {
+            o.print("HTTP/1.0 404 Not Found\r\n"+
+                "Content-type: text/html\r\n\r\n"+
+                "<html><head></head><body>HTTP/1.0 404 Not Found<br>"+file+" not found</body></html>\n");
+            o.close();
+            in.close();
+            socket.close();
+            System.out.println("connection closed");
+        }  
+    }
 }
